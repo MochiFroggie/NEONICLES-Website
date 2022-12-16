a36156cc2ca4278408e6a7c0a00be302c7d8fd8b
MochiFroggie <mochifroggie69@gmail.com>
2022-12-16 11:00:53 +0200

    Last one, swear it.

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+// Added two functions called "menu" and "menuLinks".
 const menu = document.querySelector('#mobile-menu')
 const menuLinks = document.querySelector('.navbar__menu')
+
+// Added eventListener that allows for a drop-down menu on mobile.
 
 menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
